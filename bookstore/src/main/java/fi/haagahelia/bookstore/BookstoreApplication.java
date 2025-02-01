f7d565abae182367ae7c43d186b07d78bdd880ec
tikanoja <107203742+tikanoja@users.noreply.github.com>
2025-02-01 11:55:45 +0200

    migrate to local mysql db

--- bookstore/src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
@@ -18,38 +18,46 @@ public class BookstoreApplication {
 
     @Bean
     public CommandLineRunner demo(BookRepository brepository,
-                                  CategoryRepository crepository) {
+                                CategoryRepository crepository) {
         return (args) -> {
-            Category category1 = new Category("Sci-Fi");
-            Category category2 = new Category("Horror");
-            Category category3 = new Category("Fantasy");
+            // check the entries in the repos before creating to avoid creating dups
+            // the local mySQL db is persistent between mvn run to this was always added on top
 
-            crepository.save(category1);
-            crepository.save(category2);
-            crepository.save(category3);
+            if (crepository.count() == 0) {
+                Category category1 = new Category("Sci-Fi");
+                Category category2 = new Category("Horror");
+                Category category3 = new Category("Fantasy");
 
-            Book book1 = new Book("Do Androids Dream of Electric Sheep?", "Philip K. Dick", 1968, "978-0345404473", 9.99, category1);
-            Book book2 = new Book("Snow Crash", "Neal Stephenson", 1992, "978-0553380958", 14.99, category1);
-            Book book3 = new Book("The Three-Body Problem", "Cixin Liu", 2008, "978-0765382030", 13.99, category1);
+                crepository.save(category1);
+                crepository.save(category2);
+                crepository.save(category3);
+            }
 
-            Book book4 = new Book("The Shining", "Stephen King", 1977, "978-0307743657", 12.99, category2);
-            Book book5 = new Book("At the Mountains of Madness", "H.P. Lovecraft", 1936, "978-0812974416", 8.99, category2);
-            Book book6 = new Book("The Haunting of Hill House", "Shirley Jackson", 1959, "978-0143039982", 10.99, category2);
+            if (brepository.count() == 0) {
+                Book book1 = new Book("Do Androids Dream of Electric Sheep?", "Philip K. Dick", 1968, "978-0345404473", 9.99, crepository.findByName("Sci-Fi").get(0));
+                Book book2 = new Book("Snow Crash", "Neal Stephenson", 1992, "978-0553380958", 14.99, crepository.findByName("Sci-Fi").get(0));
+                Book book3 = new Book("The Three-Body Problem", "Cixin Liu", 2008, "978-0765382030", 13.99, crepository.findByName("Sci-Fi").get(0));
 
-            Book book7 = new Book("The Hobbit", "J.R.R. Tolkien", 1937, "978-0345339683", 10.99, category3);
-            Book book8 = new Book("A Game of Thrones", "George R.R. Martin", 1996, "978-0553103540", 15.99, category3);
-            Book book9 = new Book("The Way of Kings", "Brandon Sanderson", 2010, "978-0765326355", 16.99, category3);
+                Book book4 = new Book("The Shining", "Stephen King", 1977, "978-0307743657", 12.99, crepository.findByName("Horror").get(0));
+                Book book5 = new Book("At the Mountains of Madness", "H.P. Lovecraft", 1936, "978-0812974416", 8.99, crepository.findByName("Horror").get(0));
+                Book book6 = new Book("The Haunting of Hill House", "Shirley Jackson", 1959, "978-0143039982", 10.99, crepository.findByName("Horror").get(0));
 
-            brepository.save(book1);
-            brepository.save(book2);
-            brepository.save(book3);
-            brepository.save(book4);
-            brepository.save(book5);
-            brepository.save(book6);
-            brepository.save(book7);
-            brepository.save(book8);
-            brepository.save(book9);
+                Book book7 = new Book("The Hobbit", "J.R.R. Tolkien", 1937, "978-0345339683", 10.99, crepository.findByName("Fantasy").get(0));
+                Book book8 = new Book("A Game of Thrones", "George R.R. Martin", 1996, "978-0553103540", 15.99, crepository.findByName("Fantasy").get(0));
+                Book book9 = new Book("The Way of Kings", "Brandon Sanderson", 2010, "978-0765326355", 16.99, crepository.findByName("Fantasy").get(0));
+
+                brepository.save(book1);
+                brepository.save(book2);
+                brepository.save(book3);
+                brepository.save(book4);
+                brepository.save(book5);
+                brepository.save(book6);
+                brepository.save(book7);
+                brepository.save(book8);
+                brepository.save(book9);
+            }
         };
     }
+
 
 }
